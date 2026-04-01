2b39d693d92f7366f451da2c1fa53dd95f92794a
tanyyaa90 <manetanya28@gmail.com>
2026-04-02 01:10:10 +0530

    Add test servlet to debug servlet deployment

---
+++ src/main/java/controller/TestServlet.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Test Servlet is Working!</h1>");
+        response.getWriter().println("<p>If you see this, servlets are being deployed correctly.</p>");
+    }
+}
